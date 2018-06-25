2ab9c0d044818ac5bc42cd141e4a140fb7161131
Mahesh <maheshs.manu1993@gmail.com>
2018-06-25 12:26:47 +0530

    Updated API

--- API/productlist.php
+++ API/productlist.php
@@ -7,6 +7,7 @@ class radelproductreview {
     const username = '';
     const password = '';
     const product_list='product_list';
+    const review_list='review_list';
 
     private function db($query) {
         $conn = new mysqli(self::host, self::username, self::password, self::database);
@@ -20,7 +21,9 @@ class radelproductreview {
         $table=  self::product_list;
         $query = "INSERT INTO `product_list` (`pid`,`product_name`,`product_description`) VALUES ('$pid','$product_name','$product_description')";
         $result = self::db($query);
-        return 'Success';
+        $data['success']=TRUE;
+        return json_encode($data);
+        
     }
     public static function get_product($param){
         $table=  self::product_list;
@@ -46,8 +49,9 @@ class radelproductreview {
         $product_description = params['product_description'];
         $table=  self::product_list;
         $query = "UPDATE `product_list` SET `pid`='$pid',`product_name`='$product_name',`product_description='$product_description' WHERE `id`='$pid'";
-        $result = $db->query($query) or die();
-        return 'Success';
+        $result = self::db($query);
+        $data['success']=TRUE;
+        return json_encode($data);
     }
     
     public static function add_review($params) {
@@ -59,14 +63,15 @@ class radelproductreview {
         $review_date = $rd->format('Y-m-d');
         $reviewer_name = params['reviewer_name'];
         $query = "INSERT INTO `product_review` (`pid`,`review`,`review_date`,`reviewer_name`) VALUES ('$pid','$review','$review_date','$reviewer_name')";
-        $result = $db->query($query) or die();
-        return 'Success';
+       $result = self::db($query);
+        $data['success']=TRUE;
+        return json_encode($data);
     }
     
     public static function get_reviews($param){
         $table=  self::review_list;
         $query = "SELECT * FROM `$table` WHERE $param ORDER BY id DESC LIMIT 10";
-        $result = $db->query($query) or die();
+        $result = self::db($query);
          $count = $result->num_rows;
         if ($count > 0) {
             while ($row = $result->fetch_assoc()) {
@@ -80,7 +85,7 @@ class radelproductreview {
         return $data;
        
     }
-    
+    //To be decided whether to keep this option or not
     public static function edit_review($params) {
         $db = self::db();
         $pid = params['pid'];
@@ -90,8 +95,9 @@ class radelproductreview {
         $reviewer_name = params['reviewer_name'];
         $table=  self::review_list;
         $query = "UPDATE `product_review` SET `pid`=$pid,`review`='$review',`review_date`='$review_date',`reviewer_name`='$reviewer_name' WHERE `id`='$pid'";
-        $result = $db->query($query) or die();
-        return 'Success';
+        $result = self::db($query);
+        $data['success']=TRUE;
+        return json_encode($data);
     }
 }
   ?>
